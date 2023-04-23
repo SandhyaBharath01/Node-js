56147168093387b3e3d38ada0d3c57d039c3dbcd
SandhyaBharath01 <sandhya.pujari26@gmail.com>
2023-04-23 14:34:16 +0530

    reading and writing from a file

--- temp.js
+++ temp.js
@@ -1,20 +1,41 @@
-function delay(time){
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            resolve("Done");
-        } , 1000);
-    });
-}
-async function resolve()
+const http = require('http')
+const server=http.createServer(function(req,res)
 {
-    console.log("a");
-    console.log("b");
-    await delay(2000);
-    console.log("c");
-    await delay(3000);
-    console.log("d");
-    await delay(1000);
-    console.log("e");
-   
-}
-resolve();
+      const url=req.url;
+      if(url==='/home')
+      {
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head> <title> my first page </title></head>');
+        res.write('<body><h1>Welcome to home page</h1></body');
+        res.write('<html>')
+      }
+
+      const url2=req.url;
+      if(url2==='/About')
+      {
+       
+        res.write('<html>');
+        res.write('<head> <title> my first page </title></head>');
+        res.write('<body><h1>Welcome to About page</h1></body');
+        res.write('<html>')
+      }
+
+      const url3=req.url;
+      if(url3==='/Node')
+      {
+       
+        res.write('<html>');
+        res.write('<head> <title> my first page </title></head>');
+        res.write('<body><h1>Welcome to node project page</h1></body');
+        res.write('<html>')
+      }
+
+       
+        res.write('<html>');
+        res.write('<head> <title> my first page </title></head>');
+        res.write('<body><h1>Hello from the node js server app</h1></body');
+        res.write('<html>')
+        res.end();
+});
+server.listen(1200)
