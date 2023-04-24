c930c65c9c5d002df94f31bd89e29b8048a9e043
SandhyaBharath01 <sandhya.pujari26@gmail.com>
2023-04-24 17:35:13 +0530

    middlewaves

--- files.js
+++ files.js
@@ -1,5 +1,18 @@
 const http = require('http');
-const routes = require('./routes')
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
-server.listen(8000);
+
+const express = require('express');
+
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log("In the middlewave");
+    next(); //this allows the request to continue to the next middleware in line
+});
+app.use((req,res,next)=>{
+    console.log("In the another middlewave");
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+const server = http.createServer(app);
+
+app.listen(8000);
