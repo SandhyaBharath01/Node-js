ff81deae0b10f43d88ca08e2ad380ee5ac116fb4
SandhyaBharath01 <sandhya.pujari26@gmail.com>
2023-04-24 23:41:55 +0530

    bodyparser

--- files.js
+++ files.js
@@ -1,14 +1,27 @@
 const http = require('http');
+const bodyParser = require('body-parser');
 
 const express = require('express');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended :false}));
 
-app.use((req,res,next)=>{
+app.use('/',(req,res,next)=>{
+    console.log("This always runs");
+    next();
+});
+
+app.use('/add-product',(req,res,next)=>{
     console.log("In the middlewave");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button><input type="number" name="size"><button type="submit">Add Size</button></form>')
     next(); //this allows the request to continue to the next middleware in line
 });
-app.use((req,res,next)=>{
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
     console.log("In the another middlewave");
     res.send('<h1>Hello from Express!</h1>');
 });
